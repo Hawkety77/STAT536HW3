8d09e665b03025c73df30ee70dd038ccea5fba88
Sam Lee <samlee.byu@gmail.com>
2024-10-11 23:33:18 -0600

    Create plot

---
+++ linear-model.R
@@ -0,0 +1,10 @@
+covariates = get_covariates()
+X <- covariates$X
+Y <- covariates$Y
+
+model.full <- lm(Y ~ X)
+
+X.reduced <- X[,colnames(X) != "Income.squared"]
+model.reduced <- lm(Y ~ X.reduced)
+
+anova(model.full, model.reduced)
